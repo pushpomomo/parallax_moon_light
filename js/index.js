06c7a6ddf49eee7b6bebe82db0aee7ab1218600d
pushpo Momo <146898831+pushpomomo@users.noreply.github.com>
2024-01-03 21:52:00 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+
+let bg = document.getElementById('bg');
+let moon = document.getElementById('moon');
+let mountain = document.getElementById('mountain');
+let road = document.getElementById('road');
+let text = document.getElementById('text');
+
+window.addEventListener('scroll', () => {
+    let { scrollY } = window;
+    
+    bg.style.top = -0.1 * scrollY + 'px';
+    moon.style.left = 1.1 * scrollY + 'px';
+    mountain.style.top = (0.5 +0.10 * scrollY) + 'px'; 
+    road.style.top = 0.5 * scrollY + 'px';
+    text.style.top = (120 + 1.5 * scrollY) + 'px';
+});
+
